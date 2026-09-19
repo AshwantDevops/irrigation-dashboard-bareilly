30e0574663b7f88285f3db973664d4c9844f3d9f
AshwantDevops <63289405+AshwantDevops@users.noreply.github.com>
2026-09-19 22:44:27 +0530

    Keep WhatsApp task token isolated in query string

--- api/tasks.js
+++ api/tasks.js
@@ -29,7 +29,7 @@ function publicBaseUrl(req) {
 }
 
 function taskLink(req, task) {
-  return `${publicBaseUrl(req)}/task.html?id=${encodeURIComponent(task.id)}#token=${encodeURIComponent(task.publicToken)}`;
+  return `${publicBaseUrl(req)}/task.html?id=${encodeURIComponent(task.id)}&token=${encodeURIComponent(task.publicToken)}&wa=1`;
 }
 
 function formatWhatsAppDeadline(deadline) {
